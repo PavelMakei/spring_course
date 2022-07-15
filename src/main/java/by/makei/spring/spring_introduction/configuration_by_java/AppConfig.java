00aa2ee1049cd5dca3a2b4f4f62b4499a4040505
BarmaleyGMN <pavel.makei@gmail.com>
2022-07-15 16:31:13 +0300

    bug fixing

--- src/main/java/by/makei/spring/spring_introduction/configuration_by_java/AppConfig.java
+++ src/main/java/by/makei/spring/spring_introduction/configuration_by_java/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan("spring_introduction.configuration_by_java")
+@ComponentScan("by.makei.spring.spring_introduction.configuration_by_java")
 @PropertySource("classpath:application1.properties")
 public class AppConfig {
 }
